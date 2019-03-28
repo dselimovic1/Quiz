ce1356dac7fb0ecf1420bf1398b228712605e3cb
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-03-28 21:01:53 +0100

    //

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
@@ -3,13 +3,21 @@ package ba.unsa.etf.rma.aktivnosti;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 import ba.unsa.etf.rma.R;
 
+
 public class KvizoviAkt extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
