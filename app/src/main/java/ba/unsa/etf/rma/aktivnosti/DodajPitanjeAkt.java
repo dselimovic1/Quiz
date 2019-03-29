f36e3d5273ad6f4dbc700acad6758de11ee592d2
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-03-29 22:12:26 +0100

    OnCreate metoda aktivnosti dodavanja pitanja.

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajPitanjeAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajPitanjeAkt.java
@@ -2,14 +2,39 @@ package ba.unsa.etf.rma.aktivnosti;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import ba.unsa.etf.rma.R;
 
 public class DodajPitanjeAkt extends AppCompatActivity {
 
+    private ArrayList<String> odgovori;
+    private ArrayAdapter<String> odgovoriAdapter;
+
+    private ListView odgovoriList;
+    private EditText nazivText;
+    private EditText odgovorText;
+    private Button dodaj;
+    private Button dodajTacan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_pitanje_akt);
+
+        odgovoriList = (ListView)findViewById(R.id.lvOdgovori);
+        nazivText = (EditText)findViewById(R.id.etNaziv);
+        odgovorText = (EditText)findViewById(R.id.etOdgovor);
+        dodaj = (Button)findViewById(R.id.btnDodajOdgovor);
+        dodajTacan = (Button)findViewById(R.id.btnDodajTacan);
+
+        odgovori = new ArrayList<>();
+        odgovoriAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, odgovori);
+        odgovoriList.setAdapter(odgovoriAdapter);
     }
 }
