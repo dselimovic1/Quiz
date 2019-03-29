52057f6961cf4e1775b1dfc05b9243386b644278
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-03-29 16:09:30 +0100

    Bug-ovi ispravljeni.

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajKvizAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajKvizAkt.java
@@ -12,5 +12,7 @@ public class DodajKvizAkt extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_kviz_akt);
+
+
     }
 }
--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
@@ -12,7 +13,6 @@ import java.util.ArrayList;
 
 import ba.unsa.etf.rma.R;
 import ba.unsa.etf.rma.klase.Kategorija;
-import ba.unsa.etf.rma.klase.KategorijaAdapter;
 import ba.unsa.etf.rma.klase.Kviz;
 import ba.unsa.etf.rma.klase.KvizAdapter;
 import ba.unsa.etf.rma.klase.Pitanje;
@@ -24,7 +24,8 @@ public class KvizoviAkt extends AppCompatActivity {
     private ArrayList<Kategorija> kategorije;
     private ArrayList<Kviz> kvizovi;
     private ArrayList<Pitanje> pitanja;
-    private KategorijaAdapter kategorijaAdapter;
+    private ArrayList<String> kategorijeIme;
+    private ArrayAdapter<String> kategorijeAdapter;
     private KvizAdapter kvizAdapter;
     private Spinner spinner;
     private ListView list;
@@ -34,25 +35,26 @@ public class KvizoviAkt extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner = (Spinner)findViewById(R.id.spPostojeceKategorije);
-        list = (ListView)findViewById(R.id.lvKvizovi);
+            spinner = (Spinner) findViewById(R.id.spPostojeceKategorije);
+            list = (ListView) findViewById(R.id.lvKvizovi);
 
-        kategorije = new ArrayList<>();
-        kategorije.add(new Kategorija("Svi" , null));
-        kvizovi = new ArrayList<>();
-        kvizovi.add(new Kviz("Dodaj Kviz", null,null));
-        pitanja = new ArrayList<>();
+            kategorije = new ArrayList<>();
+            kvizovi = new ArrayList<>();
+            kvizovi.add(new Kviz("Dodaj Kviz", null, null));
+            pitanja = new ArrayList<>();
 
-        kategorijaAdapter = new KategorijaAdapter(this, kategorije);
-        spinner.setAdapter(kategorijaAdapter);
+            kategorijeIme = new ArrayList<>();
+            kategorijeIme.add("Svi");
 
-        kvizAdapter = new KvizAdapter(this, kvizovi);
-        list.setAdapter(kvizAdapter);
+            kvizAdapter = new KvizAdapter(this, kvizovi);
+            list.setAdapter(kvizAdapter);
+            kategorijeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, kategorijeIme);
+            spinner.setAdapter(kategorijeAdapter);
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Intent intent = new Intent(KvizoviAkt.this, DodajKvizAkt.class);
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                    Intent intent = new Intent(KvizoviAkt.this, DodajKvizAkt.class);
                 if(position == kvizovi.size() - 1) {
                     intent.putExtra("novi", true);
                 }
@@ -65,10 +67,9 @@ public class KvizoviAkt extends AppCompatActivity {
                     intent.putExtra("nazivKviza", kvizovi.get(position).getNaziv());
                 }
                 intent.putExtra("kategorije", kategorije);
-                startActivity(intent);
-            }
-        });
+                    startActivity(intent);
+                }
+            });
     }
-
 
 }
