c2ec484ffb8606970cd6bdd869ffb082fda3c476
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-03-29 21:07:09 +0100

    Dodavanje nove kategorije.

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajKategorijuAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajKategorijuAkt.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rma.aktivnosti;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,9 +11,12 @@ import android.widget.EditText;
 import com.maltaisn.icondialog.Icon;
 import com.maltaisn.icondialog.IconDialog;
 
-import ba.unsa.etf.rma.R;
+import java.util.ArrayList;
 
-public class DodajKategorijuAkt extends AppCompatActivity implements IconDialog.Callback{
+import ba.unsa.etf.rma.R;
+import ba.unsa.etf.rma.klase.Kategorija;
+
+public class DodajKategorijuAkt extends AppCompatActivity implements IconDialog.Callback {
 
 
     private Icon[] ikone;
@@ -19,6 +24,7 @@ public class DodajKategorijuAkt extends AppCompatActivity implements IconDialog.
     private Button saveButton;
     private EditText icondID;
     private EditText imeKategorije;
+    private ArrayList<Kategorija> kategorije = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +33,36 @@ public class DodajKategorijuAkt extends AppCompatActivity implements IconDialog.
 
 
         final IconDialog dialog = new IconDialog();
-        iconDialogButton = (Button)findViewById(R.id.btnDodajIkonu);
-        saveButton = (Button)findViewById(R.id.btnDodajKategoriju);
-        icondID = (EditText)findViewById(R.id.etIkona);
-        imeKategorije = (EditText)findViewById(R.id.etNaziv);
+        iconDialogButton = (Button) findViewById(R.id.btnDodajIkonu);
+        saveButton = (Button) findViewById(R.id.btnDodajKategoriju);
+        icondID = (EditText) findViewById(R.id.etIkona);
+        imeKategorije = (EditText) findViewById(R.id.etNaziv);
 
         iconDialogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dialog.setSelectedIcons(ikone);
-                dialog.show(getSupportFragmentManager(),"icon_dialog");
+                dialog.show(getSupportFragmentManager(), "icon_dialog");
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(provjeriNaziv() == false) {
+                    imeKategorije.setBackgroundColor(Color.RED);
+                }
+                else if(provjeriIDIkone() == false) {
+                    icondID.setBackgroundColor(Color.RED);
+                }
+                else {
+                    kategorije.add(new Kategorija(imeKategorije.getText().toString(), icondID.getText().toString()));
+                    imeKategorije.setBackgroundColor(Color.WHITE);
+                    icondID.setBackgroundColor(Color.WHITE);
+                    Intent intent = new Intent(DodajKategorijuAkt.this, DodajKvizAkt.class);
+                    intent.putExtra("novaKategorija", imeKategorije.getText().toString());
+                    startActivity(intent);
+                }
             }
         });
     }
@@ -45,6 +71,18 @@ public class DodajKategorijuAkt extends AppCompatActivity implements IconDialog.
     public void onIconDialogIconsSelected(Icon[] icons) {
         ikone = icons;
         icondID.setText(Integer.toString(ikone[0].getId()));
+    }
+
+    private boolean provjeriNaziv() {
+        for(Kategorija k : kategorije) {
+            if(k.getNaziv().equals(imeKategorije.getText().toString()))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean provjeriIDIkone(){
+        return !icondID.getText().toString().equals("");
     }
 
 }
