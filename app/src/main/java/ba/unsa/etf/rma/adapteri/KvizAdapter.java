db6dd1a6b0523734053a42667c7e81b8ea2d52c1
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-05 18:37:00 +0200

    Filter klasa u adapteru.

--- app/src/main/java/ba/unsa/etf/rma/adapteri/KvizAdapter.java
+++ app/src/main/java/ba/unsa/etf/rma/adapteri/KvizAdapter.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.Filter;
 import android.widget.TextView;
 
 import com.maltaisn.icondialog.IconView;
@@ -18,10 +18,12 @@ import ba.unsa.etf.rma.klase.Kviz;
 public class KvizAdapter extends ArrayAdapter <Kviz>{
 
     private ArrayList<Kviz> kvizovi;
+    private ArrayList<Kviz> filter;
 
     public KvizAdapter(Context context, ArrayList<Kviz> kvizovi) {
         super(context, R.layout.kviz, kvizovi);
         this.kvizovi = kvizovi;
+        this.filter = kvizovi;
     }
 
     @Override
@@ -30,7 +32,7 @@ public class KvizAdapter extends ArrayAdapter <Kviz>{
         if(convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.kviz, parent, false);
         }
-        Kviz current = kvizovi.get(position);
+        Kviz current = filter.get(position);
 
         IconView ikona = (IconView)convertView.findViewById(R.id.ikona);
         ikona.setIcon(Integer.parseInt(current.getKategorija().getId()));
@@ -38,5 +40,41 @@ public class KvizAdapter extends ArrayAdapter <Kviz>{
         ime.setText(current.getNaziv());
 
         return convertView;
+    }
+
+
+    private class KvizFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            FilterResults filterResults = new FilterResults();
+
+            if(charSequence == null || charSequence.length() == 0) {
+                filterResults.count = kvizovi.size();
+                filterResults.values = kvizovi;
+            }
+            else {
+                ArrayList<Kviz> filtriranaLista = new ArrayList<>();
+                for(Kviz k : kvizovi) {
+                    if(k.getKategorija().getNaziv().equals(charSequence))
+                        filtriranaLista.add(k);
+                }
+                filterResults.count = filtriranaLista.size();
+                filterResults.values = filtriranaLista;
+            }
+
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            if(filterResults.count == 0)
+                notifyDataSetInvalidated();
+            else {
+                filter = (ArrayList<Kviz>) filterResults.values;
+                notifyDataSetChanged();
+            }
+
+        }
     }
 }
