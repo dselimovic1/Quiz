3f7f021867c022bc2bf8560f48176a98d20bf50d
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-05 17:08:20 +0200

    Novi adapter.

---
+++ app/src/main/java/ba/unsa/etf/rma/adapteri/MogucaPitanjaAdapter.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rma.adapteri;
+
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class MogucaPitanjaAdapter extends ArrayAdapter<String> {
+
+    ArrayList<String> mogucaPitanja;
+
+
+}
