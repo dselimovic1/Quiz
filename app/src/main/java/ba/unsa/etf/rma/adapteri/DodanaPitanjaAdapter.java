fad9aac543cfe024865927042237cd8733006258
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-05 17:25:47 +0200

    Završen adapter za dodana pitanja.

--- app/src/main/java/ba/unsa/etf/rma/adapteri/DodanaPitanjaAdapter.java
+++ app/src/main/java/ba/unsa/etf/rma/adapteri/DodanaPitanjaAdapter.java
@@ -1,7 +1,11 @@
 package ba.unsa.etf.rma.adapteri;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,5 +20,37 @@ public class DodanaPitanjaAdapter extends ArrayAdapter<String> {
         this.dodanaPitanja = dodanaPitanja;
     }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        String current = dodanaPitanja.get(position);
 
+        if(convertView == null) {
+            if(getItemViewType(position) == 0) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.dodana_pitanja, parent, false);
+            }
+            else {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.moguca_pitanja, parent, false);
+            }
+        }
+
+        if(getItemViewType(position) == 0) {
+            TextView textView = (TextView)convertView.findViewById(R.id.ime);
+            textView.setText(current);
+        }
+        else {
+            TextView textView = (TextView)convertView.findViewById(R.id.ime);
+            textView.setText(current);
+        }
+        return convertView;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (getCount() - 1 == position) ? 1 : 0;
+    }
 }
