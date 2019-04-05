23520fd3e192942176bca46fca92a3df6de7a12c
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-05 17:11:25 +0200

    MogucaPitanjaAdapter završen.

--- app/src/main/java/ba/unsa/etf/rma/adapteri/MogucaPitanjaAdapter.java
+++ app/src/main/java/ba/unsa/etf/rma/adapteri/MogucaPitanjaAdapter.java
@@ -1,12 +1,34 @@
 package ba.unsa.etf.rma.adapteri;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import ba.unsa.etf.rma.R;
 
 public class MogucaPitanjaAdapter extends ArrayAdapter<String> {
 
     ArrayList<String> mogucaPitanja;
 
+    MogucaPitanjaAdapter(Context context, ArrayList<String> mogucaPitanja) {
+        super(context, R.layout.moguca_pitanja, mogucaPitanja);
+        this.mogucaPitanja = mogucaPitanja;
+    }
 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        String current = mogucaPitanja.get(position);
+
+        if(convertView == null)
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.moguca_pitanja, parent,false);
+
+        TextView textView = (TextView)convertView.findViewById(R.id.ime);
+        textView.setText(current);
+        return convertView;
+    }
 }
