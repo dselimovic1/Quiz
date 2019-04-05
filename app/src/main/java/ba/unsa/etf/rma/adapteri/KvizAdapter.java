035a3b6374be3925f2b00e0aad1b116221a97e86
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-05 16:52:19 +0200

    Update klase KvizAdapter.

--- app/src/main/java/ba/unsa/etf/rma/adapteri/KvizAdapter.java
+++ app/src/main/java/ba/unsa/etf/rma/adapteri/KvizAdapter.java
@@ -17,28 +17,45 @@ import ba.unsa.etf.rma.klase.Kviz;
 
 public class KvizAdapter extends ArrayAdapter <Kviz>{
 
-    private Context context;
     private ArrayList<Kviz> kvizovi;
 
     public KvizAdapter(Context context, ArrayList<Kviz> kvizovi) {
         super(context, R.layout.kviz, kvizovi);
-
-        this.context = context;
         this.kvizovi = kvizovi;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        if(convertView == null)
-            convertView = LayoutInflater.from(context).inflate(R.layout.kviz, parent, false);
-
+        if(convertView == null) {
+            if(getItemViewType(position) == 0)
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.kviz, parent, false);
+            else
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.kviz_dodaj, parent, false);
+        }
         Kviz current = kvizovi.get(position);
 
-        IconView ikona = (IconView)convertView.findViewById(R.id.ikona);
-        ikona.setIcon(Integer.parseInt(current.getKategorija().getId()));
-        TextView ime = (TextView)convertView.findViewById(R.id.ime);
-        ime.setText(current.getNaziv());
+
+        if(getItemViewType(position) == 1) {
+            TextView ime = (TextView)convertView.findViewById(R.id.ime);
+            ime.setText(current.getNaziv());
+        }
+        else {
+            IconView ikona = (IconView)convertView.findViewById(R.id.ikona);
+            ikona.setIcon(Integer.parseInt(current.getKategorija().getId()));
+            TextView ime = (TextView)convertView.findViewById(R.id.ime);
+            ime.setText(current.getNaziv());
+        }
         return convertView;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (getCount() - 1 == position) ? 1 : 0;
     }
 }
