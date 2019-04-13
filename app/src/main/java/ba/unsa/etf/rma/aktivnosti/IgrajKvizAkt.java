36f8d738479e227b98062d36c96d032c9bdcb5b1
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-14 01:57:44 +0200

    Dodana aktivnost IgrajKvizAkt

---
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/IgrajKvizAkt.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rma.aktivnosti;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ba.unsa.etf.rma.R;
+
+public class IgrajKvizAkt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_igraj_kviz_akt);
+    }
+}
