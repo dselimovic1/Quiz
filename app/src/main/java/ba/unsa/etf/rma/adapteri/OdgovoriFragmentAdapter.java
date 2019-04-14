56b08ac9872e1380ab0c1ecfaac6c2cc17696bcb
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-14 13:00:05 +0200

    Konstruktor za adapter za odgovore.

--- app/src/main/java/ba/unsa/etf/rma/adapteri/OdgovoriFragmentAdapter.java
+++ app/src/main/java/ba/unsa/etf/rma/adapteri/OdgovoriFragmentAdapter.java
@@ -5,13 +5,17 @@ import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
+import ba.unsa.etf.rma.R;
+
 public class OdgovoriFragmentAdapter extends ArrayAdapter<String> {
 
     private ArrayList<String> odgovori;
     private int odabran = -1;
     private int tacan;
 
-    public OdgovoriFragmentAdapter(Context context, ArrayList<String> odgovori) {
-        super(context, );
+    public OdgovoriFragmentAdapter(Context context, ArrayList<String> odgovori, int tacan) {
+        super(context, R.layout.odgovor_fragment, odgovori);
+        this.odgovori = odgovori;
+        this.tacan = tacan;
     }
 }
