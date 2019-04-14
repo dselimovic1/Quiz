113430b193bffee014dbcf3e5b0b4a25be7a4c61
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-14 03:22:56 +0200

    Dodavanje fragmenta prilikom pokretanja aktivnosti IgrajKvizAkt.

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/IgrajKvizAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/IgrajKvizAkt.java
@@ -1,9 +1,12 @@
 package ba.unsa.etf.rma.aktivnosti;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import ba.unsa.etf.rma.R;
+import ba.unsa.etf.rma.fragmenti.InformacijeFrag;
+import ba.unsa.etf.rma.fragmenti.PitanjeFrag;
 
 public class IgrajKvizAkt extends AppCompatActivity {
 
@@ -11,5 +14,17 @@ public class IgrajKvizAkt extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_igraj_kviz_akt);
+
+        FragmentManager fm = getSupportFragmentManager();
+        InformacijeFrag informacijeFrag = (InformacijeFrag)fm.findFragmentById(R.id.informacijePlace);
+        PitanjeFrag pitanjeFrag = (PitanjeFrag)fm.findFragmentById(R.id.pitanjePlace);
+        if(informacijeFrag == null) {
+            informacijeFrag = new InformacijeFrag();
+            fm.beginTransaction().replace(R.id.informacijePlace, informacijeFrag).commit();
+        }
+        if(pitanjeFrag == null) {
+            pitanjeFrag = new PitanjeFrag();
+            fm.beginTransaction().replace(R.id.pitanjePlace, pitanjeFrag).commit();
+        }
     }
 }
