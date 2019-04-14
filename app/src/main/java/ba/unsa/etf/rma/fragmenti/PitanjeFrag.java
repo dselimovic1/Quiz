15f9a6d3ef0e23e6f7bb8e4b8b06d5312d411467
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-14 13:10:55 +0200

    Metode u klasi PitanjeFrag.

--- app/src/main/java/ba/unsa/etf/rma/fragmenti/PitanjeFrag.java
+++ app/src/main/java/ba/unsa/etf/rma/fragmenti/PitanjeFrag.java
@@ -7,10 +7,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import ba.unsa.etf.rma.R;
+import ba.unsa.etf.rma.klase.Kviz;
+import ba.unsa.etf.rma.klase.Pitanje;
 
 public class PitanjeFrag extends Fragment {
 
+    private Kviz k;
+    private int pozicijaTacnog;
+
     public PitanjeFrag() {
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        k = (Kviz)getActivity().getIntent().getSerializableExtra("kviz");
     }
 
     @Override
@@ -21,5 +33,12 @@ public class PitanjeFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_pitanje, container, false);
+    }
+
+    private int odrediPozicijuTacnog(Pitanje p) {
+        for(int i = 0; i < p.getOdgovori().size(); i++) {
+            if(p.getTacan().equals(p.getOdgovori().get(i))) return i;
+        }
+        return -1;
     }
 }
