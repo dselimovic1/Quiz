bc3739b69b2680424d0f2002289bcbb888ccbfee
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-16 20:53:46 +0200

    DetailFrag klasa dodana.

---
+++ app/src/main/java/ba/unsa/etf/rma/fragmenti/DetailFrag.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rma.fragmenti;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ba.unsa.etf.rma.R;
+
+public class DetailFrag extends Fragment {
+
+
+    public DetailFrag() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+}
