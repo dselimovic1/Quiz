fefd9c04647537056df7069ce8bbbb5ce4bde748
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-17 00:02:28 +0200

    Izmjena onActivityResult u kvizoviAkt.

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/KvizoviAkt.java
@@ -128,12 +128,12 @@ public class KvizoviAkt extends AppCompatActivity {
             DetailFrag detailFrag = (DetailFrag)fm.findFragmentById(R.id.detailPlace);
             if(detailFrag == null) {
                 detailFrag = new DetailFrag();
+            }
                 Bundle bundle = new Bundle();
                 kvizovi.add(new Kviz("Dodaj kviz", null, new Kategorija("", "671")));
                 bundle.putParcelableArrayList("kviz", kvizovi);
                 detailFrag.setArguments(bundle);
                 fm.beginTransaction().replace(R.id.detailPlace, detailFrag).commit();
-            }
         }
     }
 
@@ -148,6 +148,7 @@ public class KvizoviAkt extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && mode == false) {
             Kviz k = (Kviz) data.getSerializableExtra("noviKviz");
             if (requestCode == ADD_QUIZ) {
--- app/src/main/java/ba/unsa/etf/rma/fragmenti/DetailFrag.java
+++ app/src/main/java/ba/unsa/etf/rma/fragmenti/DetailFrag.java
@@ -1,21 +1,28 @@
 package ba.unsa.etf.rma.fragmenti;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
 
 import ba.unsa.etf.rma.R;
 import ba.unsa.etf.rma.adapteri.GridAdapter;
+import ba.unsa.etf.rma.aktivnosti.DodajKvizAkt;
 import ba.unsa.etf.rma.klase.Kviz;
+
+import static android.app.Activity.RESULT_OK;
 
 public class DetailFrag extends Fragment {
 
+
+    private static final int ADD_QUIZ = 1;
 
     private ArrayList<Kviz> kvizovi;
     private GridAdapter adapter;
@@ -32,11 +39,35 @@ public class DetailFrag extends Fragment {
         kvizovi = getArguments().getParcelableArrayList("kviz");
         adapter = new GridAdapter(getContext(), kvizovi);
         kvizGrid.setAdapter(adapter);
+
+        kvizGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == kvizovi.size() - 1) {
+                    Intent intent = new Intent(getActivity(), DodajKvizAkt.class);
+                    intent.putExtra("add", true);
+                    startActivityForResult(intent, ADD_QUIZ);
+                }
+            }
+        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == RESULT_OK) {
+            Kviz k = (Kviz)data.getSerializableExtra("noviKviz");
+            if(requestCode == ADD_QUIZ) {
+                int pozicija = kvizovi.size() - 1;
+                if(pozicija < 0 ) pozicija = 0;
+                kvizovi.add(pozicija, k);
+                adapter.notifyDataSetChanged();
+            }
+        }
     }
 
 }
