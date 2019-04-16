e7ecd8ae8e159a654ef148106f3a8da5888e16e9
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-16 21:57:26 +0200

    Postavljanje adaptera za listu kategorija u ListaFrag.

--- app/src/main/java/ba/unsa/etf/rma/fragmenti/ListaFrag.java
+++ app/src/main/java/ba/unsa/etf/rma/fragmenti/ListaFrag.java
@@ -6,13 +6,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import ba.unsa.etf.rma.R;
 
 public class ListaFrag extends Fragment {
 
 
+    private ArrayList<String> kategorije;
+    private ArrayAdapter<String> kategorijeAdapter;
+    private ListView listaKategorije;
+
     public ListaFrag() {
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listaKategorije = (ListView)getView().findViewById(R.id.listaKategorija);
+        kategorije = getArguments().getStringArrayList("kategorije");
+        kategorijeAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, kategorije);
+        listaKategorije.setAdapter(kategorijeAdapter);
     }
 
     @Override
