fe916b248a3ccbc39487e2e9a56b119410ee5836
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-16 23:09:51 +0200

    Kvizovi u DetailFrag.

--- app/src/main/java/ba/unsa/etf/rma/fragmenti/DetailFrag.java
+++ app/src/main/java/ba/unsa/etf/rma/fragmenti/DetailFrag.java
@@ -6,15 +6,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 import ba.unsa.etf.rma.R;
+import ba.unsa.etf.rma.klase.Kviz;
 
 public class DetailFrag extends Fragment {
 
 
+    private ArrayList<Kviz> kvizovi;
+    private GridView kvizGrid;
+
     public DetailFrag() {
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        kvizGrid = (GridView)getView().findViewById(R.id.gridKvizovi);
+
+        kvizovi = getArguments().getParcelableArrayList("kviz");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
