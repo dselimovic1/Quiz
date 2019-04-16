27a3f223ff10ffffc72decf5460b91f6ce5973df
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-16 02:10:08 +0200

    GetView metoda u adapteru.

--- app/src/main/java/ba/unsa/etf/rma/adapteri/OdgovoriFragmentAdapter.java
+++ app/src/main/java/ba/unsa/etf/rma/adapteri/OdgovoriFragmentAdapter.java
@@ -1,9 +1,11 @@
 package ba.unsa.etf.rma.adapteri;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -26,6 +28,27 @@ public class OdgovoriFragmentAdapter extends ArrayAdapter<String> {
     public View getView(int position, View convertView, ViewGroup parent) {
         String current = odgovori.get(position);
 
+        if(convertView == null) {
+            if(getItemViewType(position) == 0)
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.odgovor_u_fragmentu, parent, false);
+            else if(getItemViewType(position) == 1)
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.odgovor_u_fragmentu_green, parent, false);
+            else
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.odgovor_u_fragmentu_red, parent, false);
+        }
+
+        if(getItemViewType(position) == 0) {
+            TextView textView = (TextView)convertView.findViewById(R.id.fragmentOdg);
+            textView.setText(current);
+        }
+        else if(getItemViewType(position) == 1) {
+            TextView textView = (TextView)convertView.findViewById(R.id.fragmentOdg);
+            textView.setText(current);
+        }
+        else if(getItemViewType(position) == 2) {
+            TextView textView = (TextView)convertView.findViewById(R.id.fragmentOdg);
+            textView.setText(current);
+        }
 
         return convertView;
     }
