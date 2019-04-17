c0e0d106bb223a0991bb418e920343bf9c771c33
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-04-18 00:56:40 +0200

    Završena validacija prilikom importa.

--- app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajKvizAkt.java
+++ app/src/main/java/ba/unsa/etf/rma/aktivnosti/DodajKvizAkt.java
@@ -241,6 +241,19 @@ public class DodajKvizAkt extends AppCompatActivity {
                             return;
                         }
                     }
+                    ArrayList<String> checkPitanje = new ArrayList<>();
+                    for(int i = 1; i <= Integer.parseInt(quizData[2]); i++) {
+                        Pitanje p = izdvojiPitanje(temp.get(i).split(","));
+                        if(duplicateCheck(p.getOdgovori()) == false) {
+                            prikaziAlertDialog("Kviz kojeg importujete nije ispravan postoji ponavljanje odgovora!");
+                            return;
+                        }
+                        checkPitanje.add(p.getNaziv());
+                    }
+                    if(duplicateCheck(checkPitanje) == false) {
+                        prikaziAlertDialog("Kviz nije ispravan postoje dva pitanja sa istim nazivom!");
+                        return;
+                    }
                     izvdojiSvaPitanja(quizData, temp);
                     imeKviz.setText(quizData[0]);
                     izdvojiKategorijuImport(quizData);
@@ -275,28 +288,12 @@ public class DodajKvizAkt extends AppCompatActivity {
         dodanaAdapter.notifyDataSetChanged();
     }
 
-    private boolean provjeriPitanja(String[] quizData, ArrayList<String> temp) {
-        ArrayList<String> pitanjaIme = new ArrayList<>();
-        for(int i = 0; i < Integer.parseInt(quizData[2]); i++) {
-            Pitanje p = izdvojiPitanje(temp.get(i + 1).split(","));
-            pitanjaIme.add(p.getNaziv());
-        }
-        return duplicateCheck(pitanjaIme);
-    }
-
     private boolean duplicateCheck(ArrayList<String> temp) {
         Set<String> duplicateCheck = new HashSet<>();
         for(String s : temp) {
             if(!duplicateCheck.add(s)) return false;
         }
         return true;
-    }
-
-    private boolean provjeriOdgovore(String[] s){
-        ArrayList<String> odgovori = new ArrayList<>();
-        int brojOdgovora = Integer.parseInt(s[1]);
-        for(int i = 0; i < brojOdgovora; i++) odgovori.add(s[i + 3]);
-        return duplicateCheck(odgovori);
     }
 
     private Pitanje izdvojiPitanje(String[] s) {
