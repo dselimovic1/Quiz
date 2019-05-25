0f6f982e99d55766348822613f694eaeda72f325
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-05-25 13:39:10 +0200

    Dodan AsyncZask UpdateQuizTask.

---
+++ app/src/main/java/ba/unsa/etf/rma/taskovi/UpdateQuizTask.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rma.taskovi;
+
+import android.os.AsyncTask;
+
+import ba.unsa.etf.rma.klase.Kviz;
+
+public class UpdateQuizTask extends AsyncTask<Kviz, Void, Void> {
+    @Override
+    protected Void doInBackground(Kviz... kvizovi) {
+        return null;
+    }
+}
