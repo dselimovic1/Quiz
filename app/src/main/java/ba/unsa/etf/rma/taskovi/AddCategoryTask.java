fdc99cd453eb9d0920259b47343c89f6ed090c17
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-05-25 02:53:36 +0200

    DoInBackground metoda u AddCategoryTask završena.

--- app/src/main/java/ba/unsa/etf/rma/taskovi/AddCategoryTask.java
+++ app/src/main/java/ba/unsa/etf/rma/taskovi/AddCategoryTask.java
@@ -2,13 +2,42 @@ package ba.unsa.etf.rma.taskovi;
 
 import android.os.AsyncTask;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+
+import ba.unsa.etf.rma.helper.ConnectionHelper;
 import ba.unsa.etf.rma.klase.Kategorija;
 
 public class AddCategoryTask extends AsyncTask<Kategorija, Void, Void> {
 
+    private InputStream stream;
+    private ConnectionHelper connectionHelper = new ConnectionHelper();
+    private static String AUTH = "https://www.googleapis.com/auth/datastore";
+    private static String URL = "https://firestore.googleapis.com/v1/projects/rmaspirala-2a3e2/databases/(default)/documents/Kategorije?access_token=";
+
+    public AddCategoryTask(InputStream stream) {
+        this.stream = stream;
+    }
 
     @Override
-    protected Void doInBackground(Kategorija... kategorijas) {
+    protected Void doInBackground(Kategorija... kategorije) {
+        try {
+            String TOKEN = connectionHelper.setAccessToken(stream, AUTH);
+            HttpURLConnection conn = connectionHelper.setConnection(URL, TOKEN);
+            String document = getJSONFormat(kategorije[0]);
+            connectionHelper.writeDocument(conn, document);
+            String response = connectionHelper.getResponse(conn.getInputStream());
+            kategorije[0].setDocumendID(connectionHelper.getDocumentID(response));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String getJSONFormat(Kategorija kategorija) {
         return null;
     }
 }
