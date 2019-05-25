98886c7c67e949f75e2d5f1ea9c81ac739c4ffba
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-05-25 22:40:18 +0200

    Dodan adapter za rang listu.

---
+++ app/src/main/java/ba/unsa/etf/rma/adapteri/RangListaAdapter.java
@@ -0,0 +1,41 @@
+package ba.unsa.etf.rma.adapteri;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import ba.unsa.etf.rma.R;
+import ba.unsa.etf.rma.klase.Rang;
+
+public class RangListaAdapter extends ArrayAdapter<Rang.Par> {
+
+    private ArrayList<Rang.Par> lista;
+
+    public RangListaAdapter(Context context, ArrayList<Rang.Par> lista) {
+        super(context, R.layout.ranglista, lista);
+        this.lista = lista;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null)
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.ranglista, parent, false);
+
+        Rang.Par current = lista.get(position);
+        TextView pozicija = (TextView)convertView.findViewById(R.id.pozicija);
+        TextView igrac = (TextView)convertView.findViewById(R.id.igrac);
+        TextView procenat = (TextView)convertView.findViewById(R.id.procenat);
+        pozicija.setText(Integer.toString(position + 1) + ".");
+        igrac.setText(current.imeIgraca);
+        procenat.setText(Double.toString(current.procenatTacnih));
+        return convertView;
+    }
+}
