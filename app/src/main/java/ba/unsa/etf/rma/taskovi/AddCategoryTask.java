856a9ee6d9fcf04a9dd089c3e2e64222db12903a
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-05-25 02:46:16 +0200

    Dodan task za dodavanje kategorije.

---
+++ app/src/main/java/ba/unsa/etf/rma/taskovi/AddCategoryTask.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rma.taskovi;
+
+import android.os.AsyncTask;
+
+import ba.unsa.etf.rma.klase.Kategorija;
+
+public class AddCategoryTask extends AsyncTask<Kategorija, Void, Void> {
+
+
+    @Override
+    protected Void doInBackground(Kategorija... kategorijas) {
+        return null;
+    }
+}
