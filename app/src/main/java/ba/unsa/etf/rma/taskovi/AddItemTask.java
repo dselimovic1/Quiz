8432e6b1807a85cdd8aaf5699d824c3330c8b255
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-05-26 15:17:18 +0200

    Dodan AddItem task za dodavanje objekata u bazu.

--- app/src/main/java/ba/unsa/etf/rma/taskovi/AddItemTask.java
+++ app/src/main/java/ba/unsa/etf/rma/taskovi/AddItemTask.java
@@ -7,30 +7,33 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 
 import ba.unsa.etf.rma.helperi.ConnectionHelper;
-import ba.unsa.etf.rma.klase.Rang;
+import ba.unsa.etf.rma.interfejsi.Loadable;
+import ba.unsa.etf.rma.singleton.Baza;
 
-public class AddRangTask extends AsyncTask<Rang, Void, Void> {
+public class AddItemTask extends AsyncTask<Loadable, Void, Void> {
 
-
+    private Baza.TaskType type;
     private InputStream stream;
     private ConnectionHelper connectionHelper = new ConnectionHelper();
     private static String REQUEST_TYPE = "POST";
     private static String AUTH = "https://www.googleapis.com/auth/datastore";
-    private static String URL = "https://firestore.googleapis.com/v1/projects/rmaspirala-2a3e2/databases/(default)/documents/Pitanja?access_token=";
+    private static String URL = "https://firestore.googleapis.com/v1/projects/rmaspirala-2a3e2/databases/(default)/documents/";
 
-    public AddRangTask(InputStream stream) {
+    public AddItemTask(InputStream stream, Baza.TaskType type) {
         this.stream = stream;
+        this.type = type;
     }
 
     @Override
-    protected Void doInBackground(Rang... rangs) {
+    protected Void doInBackground(Loadable... loadables) {
         try {
+            URL = connectionHelper.setListURL(type, URL);
             String TOKEN = connectionHelper.setAccessToken(stream, AUTH);
             HttpURLConnection conn = connectionHelper.setConnection(URL, TOKEN, REQUEST_TYPE);
-            String document = rangs[0].getJSONFormat();
+            String document = loadables[0].getJSONFormat();
             connectionHelper.writeDocument(conn, document);
             String response = connectionHelper.getResponse(conn.getInputStream());
-            rangs[0].setDocumentID(connectionHelper.getDocumentID(response));
+            loadables[0].setDocumentID(connectionHelper.getDocumentID(response));
         }
         catch (IOException e) {
             e.printStackTrace();
