b0a4b68ee2f520cd69c3515e84edca35436c38d5
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-05-26 23:32:44 +0200

    Završen GetListTask.

--- app/src/main/java/ba/unsa/etf/rma/taskovi/GetListTask.java
+++ app/src/main/java/ba/unsa/etf/rma/taskovi/GetListTask.java
@@ -1,40 +1,61 @@
 package ba.unsa.etf.rma.taskovi;
 
 import android.os.AsyncTask;
-import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
 
 import ba.unsa.etf.rma.helperi.ConnectionHelper;
+import ba.unsa.etf.rma.interfejsi.FirestoreStorable;
+import ba.unsa.etf.rma.klase.Kategorija;
+import ba.unsa.etf.rma.klase.Kviz;
+import ba.unsa.etf.rma.klase.Pitanje;
+import ba.unsa.etf.rma.klase.Rang;
 import ba.unsa.etf.rma.singleton.Baza;
 
 public class GetListTask extends AsyncTask<Baza.TaskType, Void, String> {
 
 
-    private OnListResponse responseAdder;
+    private OnCategoryLoaded categoryLoaded;
+    private OnQuestionLoaded questionLoaded;
+    private OnQuizLoaded quizLoaded;
     private InputStream stream;
+    private Baza.TaskType type;
     private ConnectionHelper connectionHelper = new ConnectionHelper();
     private static String REQUEST_TYPE = "GET";
     private static String AUTH = "https://www.googleapis.com/auth/datastore";
-    private static String URL = "https://firestore.googleapis.com/v1/projects/rmaspirala-2a3e2/databases/(default)/documents/";
+    private String URL = "https://firestore.googleapis.com/v1/projects/rmaspirala-2a3e2/databases/(default)/documents/";
 
-    public GetListTask(InputStream stream, OnListResponse responseAdder) {
+    public GetListTask(InputStream stream, OnQuizLoaded quizLoaded) {
         this.stream = stream;
-        this.responseAdder = responseAdder;
+        this.quizLoaded = quizLoaded;
+    }
+
+    public GetListTask(InputStream stream, OnQuestionLoaded questionLoaded) {
+        this.stream = stream;
+        this.questionLoaded = questionLoaded;
+    }
+
+    public GetListTask(InputStream stream, OnCategoryLoaded categoryLoaded) {
+        this.stream = stream;
+        this.categoryLoaded = categoryLoaded;
     }
 
     @Override
     protected String doInBackground(Baza.TaskType... enums) {
         String response = null;
         try {
-            URL = connectionHelper.setListURL(enums[0], URL);
+            type = enums[0];
+            URL = connectionHelper.setListURL(type, URL);
             String TOKEN = connectionHelper.setAccessToken(stream, AUTH);
             HttpURLConnection conn = connectionHelper.setConnection(URL, TOKEN, REQUEST_TYPE);
             response = connectionHelper.getResponse(conn.getInputStream());
-            Log.d("TOKEN", TOKEN);
-            Log.d("RESPONSE",response);
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -44,10 +65,57 @@ public class GetListTask extends AsyncTask<Baza.TaskType, Void, String> {
 
     @Override
     protected void onPostExecute(String response) {
-        responseAdder.setResponse(response);
+        ArrayList<FirestoreStorable> store = new ArrayList<>();
+        try {
+            JSONObject obj = new JSONObject(response);
+            JSONArray array = obj.getJSONArray("documents");
+            for (int i = 0; i < array.length(); i++) {
+                store.add(storeValue(array.getJSONObject(i)));
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+        callback(store);
     }
 
-    public interface OnListResponse {
-        void setResponse(String response);
+    private void callback(ArrayList<? extends FirestoreStorable> store) {
+        switch (type) {
+            case QUESTION:
+                questionLoaded.loadAllQuestion((ArrayList<Pitanje>) store);
+                break;
+            case QUIZ:
+                quizLoaded.loadAllQuiz((ArrayList<Kviz>) store);
+                break;
+            case CATEGORY:
+                categoryLoaded.loadAllCategory((ArrayList<Kategorija>) store);
+                break;
+        }
+    }
+
+    private FirestoreStorable storeValue(JSONObject json) {
+        switch (type) {
+            case RANGLIST:
+                return Rang.convertFromJSON(json);
+            case QUIZ:
+                return Kviz.convertFromJSON(json);
+            case CATEGORY:
+                return Kategorija.convertFromJSON(json);
+            case QUESTION:
+                return Pitanje.convertFromJSON(json);
+        }
+        throw new IllegalArgumentException("Wrong Type");
+    }
+
+    public interface OnQuestionLoaded {
+        void loadAllQuestion(ArrayList<Pitanje> load);
+    }
+
+    public interface OnQuizLoaded {
+        void loadAllQuiz(ArrayList<Kviz> load);
+    }
+
+    public interface OnCategoryLoaded {
+        void loadAllCategory(ArrayList<Kategorija> load);
     }
 }
