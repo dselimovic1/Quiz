32de1219efe22110c6d3ae256dc631672ff5e09d
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-05-27 03:45:36 +0200

    Dodana klasa JSONQuizConverter.

---
+++ app/src/main/java/ba/unsa/etf/rma/helperi/JSONQuizConverter.java
@@ -0,0 +1,39 @@
+package ba.unsa.etf.rma.helperi;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import ba.unsa.etf.rma.klase.Kategorija;
+import ba.unsa.etf.rma.klase.Pitanje;
+
+public class JSONQuizConverter {
+
+    public static String findID(String name) {
+        String[] atr = name.split("/");
+        return atr[atr.length - 1];
+    }
+
+    public static String findName(JSONObject json) throws JSONException {
+        return json.getString("stringValue");
+    }
+
+    public static ArrayList<Pitanje> getQuestions(JSONArray array) throws JSONException {
+        ArrayList<Pitanje> pitanja = new ArrayList<>();
+        for(int i = 0; i < array.length(); i++) {
+            Pitanje p = new Pitanje();
+            p.setDocumentID(findName(array.getJSONObject(i)));
+            pitanja.add(p);
+        }
+        return pitanja;
+    }
+
+    public static Kategorija getCategory(JSONObject json) throws JSONException {
+        String ID = findName(json);
+        Kategorija k = new Kategorija();
+        k.setDocumentID(ID);
+        return k;
+    }
+}
--- app/src/main/java/ba/unsa/etf/rma/klase/Kviz.java
+++ app/src/main/java/ba/unsa/etf/rma/klase/Kviz.java
@@ -3,7 +3,6 @@ package ba.unsa.etf.rma.klase;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -121,32 +120,6 @@ public class Kviz implements Parcelable, FirestoreStorable {
         json += "]}},\"idKategorije\": {\"stringValue\": \"" + getKategorija().getDocumentID() + "\"}," +
                 "\"naziv\": {\"stringValue\": \"" + getNaziv() + "\"}}}";
         return json;
-    }
-
-    private static String findID(String name) {
-        String[] atr = name.split("/");
-        return atr[atr.length - 1];
-    }
-
-    private static String findName(JSONObject json) throws JSONException {
-        return json.getString("stringValue");
-    }
-
-    private static ArrayList<Pitanje> getQuestions(JSONArray array) throws JSONException {
-        ArrayList<Pitanje> pitanja = new ArrayList<>();
-        for(int i = 0; i < array.length(); i++) {
-            Pitanje p = new Pitanje();
-            p.setDocumentID(findName(array.getJSONObject(i)));
-            pitanja.add(p);
-        }
-        return pitanja;
-    }
-
-    private static Kategorija getCategory(JSONObject json) throws JSONException {
-        String ID = findName(json);
-        Kategorija k = new Kategorija();
-        k.setDocumentID(ID);
-        return k;
     }
 
     public static Kviz convertFromJSON(JSONObject json) {
