43532b3e9530663fc1658de8f3f5735719417ca4
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-05-27 02:51:58 +0200

    Convert iz JSON u klasi Kviz.

--- app/src/main/java/ba/unsa/etf/rma/klase/Kviz.java
+++ app/src/main/java/ba/unsa/etf/rma/klase/Kviz.java
@@ -3,6 +3,8 @@ package ba.unsa.etf.rma.klase;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -121,7 +123,44 @@ public class Kviz implements Parcelable, FirestoreStorable {
         return json;
     }
 
+    private static String findID(String name) {
+        String[] atr = name.split("/");
+        return atr[atr.length - 1];
+    }
+
+    private static String findName(JSONObject json) throws JSONException {
+        return json.getString("stringValue");
+    }
+
+    private static ArrayList<Pitanje> getQuestions(JSONArray array) throws JSONException {
+        ArrayList<Pitanje> pitanja = new ArrayList<>();
+        for(int i = 0; i < array.length(); i++) {
+            Pitanje p = new Pitanje();
+            p.setDocumentID(findName(array.getJSONObject(i)));
+            pitanja.add(p);
+        }
+        return pitanja;
+    }
+
+    private static Kategorija getCategory(JSONObject json) throws JSONException {
+        String ID = findName(json);
+        Kategorija k = new Kategorija();
+        k.setDocumentID(ID);
+        return k;
+    }
+
     public static Kviz convertFromJSON(JSONObject json) {
+        Kviz kviz = new Kviz();
+        try {
+            JSONObject fields = json.getJSONObject("fields");
+            kviz.setDocumentID(findID(json.getString("name")));
+            kviz.setNaziv(findName(fields.getJSONObject("naziv")));
+            kviz.setPitanja(getQuestions(fields.getJSONObject("pitanja").getJSONObject("arrayValue").getJSONArray("values")));
+            kviz.setKategorija(getCategory(fields.getJSONObject("idKategorije")));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
