9092f30f3092c1ace80834dcd8d38afcc9bd2ff6
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-05-30 01:41:07 +0200

    Baza je sada enum.

--- app/src/main/java/ba/unsa/etf/rma/singleton/Baza.java
+++ app/src/main/java/ba/unsa/etf/rma/singleton/Baza.java
@@ -1,26 +1,7 @@
 package ba.unsa.etf.rma.singleton;
 
-import java.util.ArrayList;
-
-import ba.unsa.etf.rma.klase.Rang;
-
-
 public class Baza  {
 
     public enum TaskType {QUIZ, CATEGORY, QUESTION, RANGLIST};
-
-    private static Baza instance = new Baza();
-    private static ArrayList<Rang> rangListe = new ArrayList<>();
-
-
-    private Baza() {}
-
-    public static Baza getInstance()
-    {
-        return instance;
-    }
-
-
-
 
 }
