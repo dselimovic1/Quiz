575de06f68d628d1205c92490de799fa14b287f4
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-05-31 22:12:13 +0200

    Implementirane funkcionalnosti rang liste.

--- app/src/main/java/ba/unsa/etf/rma/fragmenti/RangLista.java
+++ app/src/main/java/ba/unsa/etf/rma/fragmenti/RangLista.java
@@ -21,8 +21,7 @@ public class RangLista extends Fragment {
     private ListView rangList;
     private RangListaAdapter adapter;
 
-    public RangLista() {
-    }
+    public RangLista() { }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
