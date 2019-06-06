dad2f220924e082788502c26b9a105727d41d8cf
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-07 01:21:01 +0200

    Implementirana klasa DatabaseHelper.

--- app/src/main/java/ba/unsa/etf/rma/sqlite/DatabaseHelper.java
+++ app/src/main/java/ba/unsa/etf/rma/sqlite/DatabaseHelper.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import ba.unsa.etf.rma.klase.Kategorija;
+import ba.unsa.etf.rma.klase.Kviz;
+import ba.unsa.etf.rma.klase.Pitanje;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "baza.db";
@@ -15,11 +19,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL(Kategorija.KategorijaEntry.CREATE_TABLE);
+        sqLiteDatabase.execSQL(Kviz.KvizEntry.CREATE_TABLE);
+        sqLiteDatabase.execSQL(Pitanje.PitanjeEntry.CREATE_TABLE);
+        sqLiteDatabase.execSQL(Kviz.PitanjaKvizaEntry.CREATE_TABLE);
+        sqLiteDatabase.execSQL(Pitanje.OdgovorEntry.CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL(Pitanje.OdgovorEntry.DROP_TABLE);
+        sqLiteDatabase.execSQL(Kviz.PitanjaKvizaEntry.DROP_TABLE);
+        sqLiteDatabase.execSQL(Pitanje.PitanjeEntry.DROP_TABLE);
+        sqLiteDatabase.execSQL(Kviz.KvizEntry.DROP_TABLE);
+        sqLiteDatabase.execSQL(Kategorija.KategorijaEntry.DROP_TABLE);
+        onCreate(sqLiteDatabase);
     }
 }
