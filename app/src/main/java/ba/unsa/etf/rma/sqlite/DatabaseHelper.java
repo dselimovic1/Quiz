72a0773ab1060b7635217a1048fc696ccd29f7cc
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-07 01:14:32 +0200

    Dodana klasa DbHelper za rad sa SQLite bazom.

---
+++ app/src/main/java/ba/unsa/etf/rma/sqlite/DatabaseHelper.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rma.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "baza.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
