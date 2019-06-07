e9771f3e0d23120f4fa962e66ae2fe6ca16f79fc
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-07 17:14:54 +0200

    Refactoring u klasi Query.

--- app/src/main/java/ba/unsa/etf/rma/sqlite/Query.java
+++ app/src/main/java/ba/unsa/etf/rma/sqlite/Query.java
@@ -3,7 +3,10 @@ package ba.unsa.etf.rma.sqlite;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+
 import ba.unsa.etf.rma.klase.Kategorija;
+import ba.unsa.etf.rma.klase.Kviz;
 import ba.unsa.etf.rma.klase.Pitanje;
 
 public class Query {
@@ -14,8 +17,15 @@ public class Query {
         this.database = database;
     }
 
-    public static String getAnswerFromCuror(Cursor cursor) {
-        return cursor.getString(0);
+    public ArrayList<String> getAnswersByID(long ID) {
+        ArrayList<String> answers = new ArrayList<>();
+        String selection = Pitanje.OdgovorEntry.PROJECTION[1] + " = ?";
+        String[] selectionArgs = new String[]{Long.toString(ID)};
+        Cursor cursor = database.query(Pitanje.OdgovorEntry.TABLE_NAME, Pitanje.OdgovorEntry.PROJECTION, selection, selectionArgs, null, null, null);
+        while(cursor.moveToNext()) {
+            answers.add(cursor.getString(0));
+        }
+        return answers;
     }
 
     public Pitanje getQuestionFromCursor(Cursor cursor) {
@@ -23,12 +33,7 @@ public class Query {
         pitanje.setID(cursor.getLong(0));
         pitanje.setNaziv(cursor.getString(1));
         pitanje.setTekstPitanja(pitanje.getNaziv());
-        String selection = Pitanje.OdgovorEntry.PROJECTION[1] + " = ?";
-        String[] selectionArgs = new String[]{Long.toString(pitanje.getID())};
-        Cursor answerCursor = database.query(Pitanje.OdgovorEntry.TABLE_NAME, Pitanje.OdgovorEntry.PROJECTION, selection, selectionArgs, null, null, null);
-        while(answerCursor.moveToNext()) {
-            pitanje.dodajOdgovor(getAnswerFromCuror(answerCursor));
-        }
+        pitanje.setOdgovori(getAnswersByID(pitanje.getID()));
         pitanje.setTacan(pitanje.getOdgovori().get(cursor.getInt(2)));
         return pitanje;
     }
@@ -40,4 +45,11 @@ public class Query {
         kategorija.setId(cursor.getColumnName(2));
         return kategorija;
     }
+
+    public Kviz getQuizFromCursor(Cursor cursor) {
+        Kviz kviz = new Kviz();
+        return kviz;
+    }
+
+
 }
