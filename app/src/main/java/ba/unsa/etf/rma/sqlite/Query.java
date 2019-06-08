71329cbc4d8856ee45b1824d1a3436df616dcd64
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-09 00:34:22 +0200

    Funkcija za azuiranje kviza.

--- app/src/main/java/ba/unsa/etf/rma/sqlite/Query.java
+++ app/src/main/java/ba/unsa/etf/rma/sqlite/Query.java
@@ -164,4 +164,15 @@ public class Query {
             database.insert(Kviz.PitanjaKvizaEntry.TABLE_NAME, null, quiz.setContentValuesForQuestion(question.getID()));
         }
     }
+
+    public void updateQuiz(Kviz quiz) {
+        String whereClause = Kviz.PitanjaKvizaEntry.COLUMN_QUIZ_ID + " = ?";
+        String updateClause = Kviz.KvizEntry.COLUMN_ID + " = ?";
+        String[] whereArgs = new String[]{Long.toString(quiz.getID())};
+        database.delete(Kviz.PitanjaKvizaEntry.TABLE_NAME, whereClause, whereArgs);
+        database.update(Kviz.KvizEntry.TABLE_NAME, quiz.getContentValues(), updateClause, whereArgs);
+        for(Pitanje question : quiz.getPitanja()) {
+            database.insert(Kviz.PitanjaKvizaEntry.TABLE_NAME, null, quiz.setContentValuesForQuestion(question.getID()));
+        }
+    }
 }
