7d3370a96e778f3b122ca7f7cb5cbcdc725dcfa8
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-09 23:29:57 +0200

    Pomoćna metoda u klasi LocalDBHelper.

--- app/src/main/java/ba/unsa/etf/rma/helperi/LocalDBHelper.java
+++ app/src/main/java/ba/unsa/etf/rma/helperi/LocalDBHelper.java
@@ -3,18 +3,18 @@ package ba.unsa.etf.rma.helperi;
 import java.util.ArrayList;
 import java.util.ListIterator;
 
-import ba.unsa.etf.rma.klase.Kviz;
+import ba.unsa.etf.rma.interfejsi.FirestoreStorable;
 
 public class LocalDBHelper {
 
-    public static ArrayList<Kviz> getQuizzesToAdd(ArrayList<Kviz> firestore, ArrayList<Kviz> local) {
-        ArrayList<Kviz> temp = new ArrayList<>(firestore);
-        ListIterator<Kviz> listIterator = temp.listIterator();
+    public static ArrayList<? extends FirestoreStorable> getEntriesToAdd(ArrayList<? extends FirestoreStorable> firestore, ArrayList<? extends FirestoreStorable> local) {
+        ArrayList<? extends FirestoreStorable> temp = new ArrayList<>(firestore);
+        ListIterator<? extends FirestoreStorable> listIterator = temp.listIterator();
         while (listIterator.hasNext()) {
-            boolean remove = true;
-            for(Kviz quiz : local) {
-                if(listIterator.next().getDocumentID().equals(quiz.getDocumentID())) {
-                    remove = false;
+            boolean remove = false;
+            for(FirestoreStorable firestoreStorable : local) {
+                if(listIterator.next().getDocumentID().equals(firestoreStorable.getDocumentID())) {
+                    remove = true;
                     break;
                 }
             }
