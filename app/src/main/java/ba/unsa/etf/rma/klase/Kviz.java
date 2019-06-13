4558f5c5bac8d57ba45084f8557bba19335a9985
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-13 20:48:31 +0200

    Izmjene u klasi Kviz.

--- app/src/main/java/ba/unsa/etf/rma/klase/Kviz.java
+++ app/src/main/java/ba/unsa/etf/rma/klase/Kviz.java
@@ -185,9 +185,9 @@ public class Kviz implements Parcelable, FirestoreStorable {
                 COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 COLUMN_NAME + " TEXT NOT NULL," +
                 COLUMN_CATEGORY_ID + " INTEGER," +
-                DOCUMENT_ID + " TEXT NOT NULL," +
-                "FOREIGN KEY (" + COLUMN_CATEGORY_ID + ") REFERENCES " + Kategorija.KategorijaEntry.TABLE_NAME +
-                "(" + Kategorija.KategorijaEntry.COLUMN_ID + ") ON DELETE CASCADE);";
+                DOCUMENT_ID + " TEXT NOT NULL);";
+                /*"FOREIGN KEY (" + COLUMN_CATEGORY_ID + ") REFERENCES " + Kategorija.KategorijaEntry.TABLE_NAME +
+                "(" + Kategorija.KategorijaEntry.COLUMN_ID + ") ON DELETE CASCADE);";*/
         public static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
 
         public static final String[] PROJECTION = new String[]{COLUMN_ID, COLUMN_NAME, COLUMN_CATEGORY_ID, DOCUMENT_ID};
