0c45569135ea1e05797733f4c074fcf6fd533491
dselimovic1 <dselimovic1@etf.unsa.ba>
2019-06-14 03:18:31 +0200

    Odgovor na zahtjev implementiran u DetailFrag.

--- app/src/main/java/ba/unsa/etf/rma/fragmenti/DetailFrag.java
+++ app/src/main/java/ba/unsa/etf/rma/fragmenti/DetailFrag.java
@@ -211,6 +211,14 @@ public class DetailFrag extends Fragment implements GetListTask.OnCategoryLoaded
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                checkEvents();
+            }
+        }
+    }
+    @Override
     public void onDestroy() {
         databaseHelper.close();
         super.onDestroy();
